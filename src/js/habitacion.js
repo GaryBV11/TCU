d4772829d971aada37202c61349b54ca8715f9b7
Gary Barahona <garybarahona85@gmail.com>
2023-02-25 20:59:17 -0600

    galeria habitacion y cambios index

---
+++ src/js/habitacion.js
@@ -0,0 +1,7 @@
+(function() {
+  Galleria.loadTheme('galleria-1.6.1/dist/themes/azur/galleria.azur.min.js');
+ // Galleria.loadTheme('galleria-1.6.1/dist/themes/twelve/galleria.twelve.min.js');
+ // Galleria.loadTheme('galleria-1.6.1/dist/themes/miniml/galleria.miniml.min.js');
+ // Galleria.loadTheme('galleria-1.6.1/dist/themes/classic/galleria.classic.min.js');
+  Galleria.run('.galleria');
+}());
